8ec885d1215d2ee38283a6309c860ff37aedcd60
12khabdouabdou <zaaaabd7@gmail.com>
2025-11-29 14:25:43 +0100

    Delete app directory

--- app/src/main/java/de/robv/android/xposed/IXposedHookLoadPackage.java
+++
@@ -1,5 +0,0 @@
-package de.robv.android.xposed;
-import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
-public interface IXposedHookLoadPackage {
-    void handleLoadPackage(LoadPackageParam lpparam) throws Throwable;
-}
--- app/src/main/java/de/robv/android/xposed/XposedHelpers.java
+++
@@ -1,9 +0,0 @@
-package de.robv.android.xposed;
-import java.lang.reflect.Member;
-public final class XposedHelpers {
-    public static XC_MethodHook.Unhook findAndHookMethod(Class<?> clazz, String methodName, Object... parameterTypesAndCallback) { return null; }
-    public static XC_MethodHook.Unhook findAndHookMethod(String className, ClassLoader classLoader, String methodName, Object... parameterTypesAndCallback) { return null; }
-    public static Object callMethod(Object obj, String methodName, Object... args) { return null; }
-    public static Object callStaticMethod(Class<?> clazz, String methodName, Object... args) { return null; }
-    public static Class<?> findClass(String className, ClassLoader classLoader) { return null; }
-}
